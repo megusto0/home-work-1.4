6a01b594406509e6e0d6ff2f5ab38d571727cf4c
megusto0 <ruslan00900@gmail.com>
2022-08-03 02:44:26 +0400

    Задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        for (;i< 10;) {
+        //Задание 1
+        int i = 1;
+        while(i <= 10) {
+            System.out.print(i + " ");
             ++i;
-            System.out.println("Hello world!");
         }
+        System.out.println();
+        for (;i > 1;) {
+            --i;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //Задание 2
+
+        int firstFriday = 7;
+
+        while(firstFriday < 31) {
+            System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");
+            firstFriday += 7;
+        }
+
+        //Задание 3
+        int currentYear = 2022;
+        int year = 0;
+        while (year < currentYear) {
+            if (year > currentYear - 200) {
+                System.out.println(year);
+            }
+            year += 79;
+        }
+        System.out.println(year);
     }
 }
