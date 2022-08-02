09d530606c23fe55148a183eaff9f9d75fbf3295
megusto0 <ruslan00900@gmail.com>
2022-08-03 02:16:29 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        for (;i< 10;) {
+            ++i;
+            System.out.println("Hello world!");
+        }
+    }
+}
